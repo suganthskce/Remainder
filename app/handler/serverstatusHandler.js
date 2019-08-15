7a730366093b7d4677075a57595958e1fcd70165
Suganth <suganth@technogramsolutions.com>
2019-08-15 19:28:48 +0530

    Added console

--- app/handler/serverstatusHandler.js
+++ app/handler/serverstatusHandler.js
@@ -4,6 +4,7 @@ const client = require('twilio')();
 
 const serverstatusHandler = async (request, reply) => {
     //aerospikeConnect();
+    console.log("Server status.....");
     const to1 = '+919942233381';
     const chottu = '+917092542919';
     const preveen = '+917845584181';
