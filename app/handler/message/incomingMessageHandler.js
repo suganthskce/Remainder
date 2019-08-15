b53d2f535acfb3c982117b350823f086085a82ac
Suganth <suganth@technogramsolutions.com>
2019-08-15 19:41:08 +0530

    Adding reply

--- app/handler/message/incomingMessageHandler.js
+++ app/handler/message/incomingMessageHandler.js
@@ -4,6 +4,7 @@ const incomingMessageHandler = async (request, reply) => {
     const { body = {} } = request;
     console.log("Incoming message:::::::::");
     console.log(`${JSON.stringify(body)}`);
+    reply.send({ message: "Message Displayed" });
 }
 
 module.exports = incomingMessageHandler;
--- app/handler/message/messageStatusHandler.js
+++ app/handler/message/messageStatusHandler.js
@@ -4,6 +4,7 @@ const messageStatusHandler = async (request, reply) => {
     const { body = {} } = request;
     console.log("Message Status:::::::::");
     console.log(`${JSON.stringify(body)}`);
+    reply.send({ message: "Message Displayed" });
 }
 
 module.exports = messageStatusHandler;
