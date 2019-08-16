bc4829843918fc94e260f96e64ca96612205bb14
Suganth <suganth@technogramsolutions.com>
2019-08-16 19:00:49 +0530

    Added parser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let fastify = require("fastify")();
 const bootstrap = require("./app/config/bootstrap");
 
 bootstrap.init(fastify);
-
+fastify.register(require("fastify-formbody"));
 const PORT = 8080;
 
 fastify.register(require("fastify-cors"), { methods: ['OPTION', 'GET', 'PUT', 'POST'], exposedHeaders: ['Content-Type', 'Authorization'] });
