1e517abd31a9899f681d3cebdc29576aee2e95ea
Suganth <suganth@technogramsolutions.com>
2019-08-17 12:34:16 +0530

    Removed message

--- app/handler/message/incomingMessageHandler.js
+++ app/handler/message/incomingMessageHandler.js
@@ -3,14 +3,18 @@ const incomingMessageHandler = async (request, reply) => {
 
     const { body = {} } = request;
     console.log("Incoming message:::::::::");
+    //const ddd = JSON.parse(JSON.stringify(body));
+    //console.log("ddd", ddd);
+    //const { Bodydsad = '', asd = '' } = ddd;
+    //console.log("Bodydsad", Bodydsad);
     console.log(`${JSON.stringify(body)}`);
 
-    const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-    reply.header('Content-Type', 'text/xml');
-    const response = new MessagingResponse();
-    response.message('Message Delivered....');
-    reply.send(response.toString());
+    //const MessagingResponse = require('twilio').twiml.MessagingResponse;
+    reply.send('');
+    //reply.header('Content-Type', 'text/xml');
+    //const response = new MessagingResponse();
+    //response.message('Message Delivered....');
+    //reply.send(response.toString());
 }
 
 module.exports = incomingMessageHandler;
