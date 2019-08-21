e8cb817e81855ad4a1cdad1df0e18b2bba959306
Suganth <suganth@technogramsolutions.com>
2019-08-21 23:38:52 +0530

    Key for wallet

--- app/handler/message/incomingMessageHandler.js
+++ app/handler/message/incomingMessageHandler.js
@@ -1,11 +1,13 @@
 const { isEmpty } = require("lodash");
 const processEntities = require("../../processor/message/entities");
 const sendMessage = require("./../../connector/telegramApi/sendMessage");
+const { logger } = require("./../../lib/logger");
 
 const incomingMessageHandler = async (request, reply) => {
 
     const { body = {} } = request;
     const data = JSON.parse(JSON.stringify(body));
+    logger.info(data);
     const { update_id = '', message = {} } = data;
     const { message_id = '', from = {}, chat = {}, date = '', text = '', entities = [] } = message;
     if (!isEmpty(entities)) {
@@ -15,6 +17,7 @@ const incomingMessageHandler = async (request, reply) => {
             case "WALLET":
                 sendMessage(from.id, `Key: WALLET`);
                 break;
+            case "ERROR":
             default:
                 sendMessage(from.id, 'Invalid Input');
         }
@@ -25,11 +28,32 @@ const incomingMessageHandler = async (request, reply) => {
     reply.send('');
 }
 
-
+const pattern = {
+    WALLET: [/#w/i, /#wallet/i]
+}
 
 
 const getKeyFromMessage = (text) => {
-
+    const keys = Object.keys(pattern);
+    const length = keys.length;
+    for (let i = 0; i < length; i++) {
+        logger.info(`Checking with ${keys[i]} patterns`);
+        const patternArray = pattern[keys[i]];
+        const patternLength = patternArray.length;
+        let matched = false;
+        for (let j = 0; j < patternLength; j++) {
+            // Change conditions
+            if (patternArray[j].test(text)) {
+                matched = true;
+            }
+        }
+        if (matched) {
+            logger.info(`Matched with key ${keys[i]}`);
+            return keys[i];
+        }
+    }
+    logger.info(`No pattern Found`);
+    return 'ERROR';
 }
 
 module.exports = incomingMessageHandler;
