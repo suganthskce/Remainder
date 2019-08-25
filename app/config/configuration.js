772b321e2906779a86d41b983417c5e72d9ad747
Suganth <suganth@technogramsolutions.com>
2019-08-25 17:47:25 +0530

    Add config

--- app/config/configuration.js
+++ app/config/configuration.js
@@ -1,7 +1,9 @@
 const nconf = require('nconf');
 const nodeEnv = process.env.NODE_ENV || 'development';
+const apiKey = process.env.BOTAPIKEY || '';
 
 nconf.argv().env();
 nconf.file("config", `app/config/env/${nodeEnv}.json`);
+nconf.set('apiKey', apiKey);
 
 module.exports = nconf;
--- app/connector/telegramApi/sendMessage.js
+++ app/connector/telegramApi/sendMessage.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 const { logger } = require("./../../lib/logger");
+const config = require('./../../config/configuration');
+
 const sendMessage = (chat_id, text) => {
     logger.info(`Sending message to user[${chat_id}]: ${text}`);
     const options = {
-        url: `https://api.telegram.org/${process.env.BOTAPIKEY}/sendMessage`,
+        url: `https://api.telegram.org/${config.get('apiKey')}/sendMessage`,
         method: 'POST',
         data: {
             chat_id,
