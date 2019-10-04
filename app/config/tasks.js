b6c924ea1e862c32c39ba7e32d69beed99d35901
Suganth <suganth@technogramsolutions.com>
2019-10-04 19:28:23 +0530

    Stock Price Fethcing

--- app/config/tasks.js
+++ app/config/tasks.js
@@ -1,31 +1,55 @@
 /**
  * This config file is to fetch the access control configuration for each role and user
  */
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const stock = require('./constants/stock.json');
+const { logger } = require("./../lib/logger");
 
-
-const getAclConfig = async () => {
-
+const getStock = async () => {
+    logger.info(`Inside getStock`);
+    const { stockList = [] } = stock;
+    stockList.forEach(data => {
+        const { id = '', url = '', fetchData = [], chatId = [] } = data;
+        logger.info(`Fetching stock for id`);
+        puppeteer
+            .launch()
+            .then(browser => browser.newPage())
+            .then(page => {
+                logger.info(`Loading page`);
+                return page.goto(url).then(function () {
+                    return page.content();
+                });
+            })
+            .then(html => {
+                logger.info(`Loading HTML`);
+                const $ = cheerio.load(html);
+                let result = {};
+                let message = `${id}\n`;
+                fetchData.forEach(content => {
+                    const { key = '', selector = '' } = content;
+                    const value = $(selector).text();
+                    result[key] = value;
+                    message = message + `[${key}]:${value}\n`;
+                });
+                chatId.forEach(chat => {
+                    sendMessage(chat, message);
+                });
+                logger.info(`Result: ${JSON.stringify(result)}`);
+            })
+            .catch(console.error);
+    });
 }
 
 setConfigInterval = () => {
     setInterval(() => {
-        getAclConfig();
-    }, 120000);
-}
-saveUserGroup = (formOptionsConfig) => {
-    global.formOptions = global.formOptions || {};
-    const { documents = [] } = formOptionsConfig;
-    documents.map((optionConfig) => {
-        const { key = "", data = [], enabled = true } = optionConfig;
-        if (enabled) {
-            global.formOptions[key] = data['data'];
-        }
-    });
+        getStock();
+    }, 1000 * 60 * 60 * 24);
 }
 
 const initialize = () => {
-    // getAclConfig();
-    // setConfigInterval();
+    getStock();
+    setConfigInterval();
 }
 
 module.exports = { initialize };
